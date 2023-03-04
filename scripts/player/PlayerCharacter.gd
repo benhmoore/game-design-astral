1ce380bc5985453df13bb67c3e122d49748355bf
Ben Moore <ben@benhmoore.com>
2023-03-04 11:19:23 -0600

    Added health danger sound effect

--- scripts/player/PlayerCharacter.gd
+++ scripts/player/PlayerCharacter.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 
 signal health_changed # Emitted whenever heal() or damage() is called
+signal player_death
 
 export var max_speed = 300
 export var acceleration = 2000
@@ -16,8 +17,32 @@ var health:float = 6 setget set_health
 onready var shoot_audio_player = get_node("ShootAudioPlayer")
 onready var footsteps_audio_player = get_node("FootstepsAudioPlayer")
 onready var effect_audio_player = get_node("EffectAudioPlayer")
+onready var danger_audio_player = get_node("DangerAudioPlayer")
 
 var motion = Vector2.ZERO
+
+var low_health_timer = 0
+func _process(delta):
+	low_health_timer += delta
+	if low_health_timer > 0.1:
+		if health < 3 and health > 0:
+			if low_health_timer >= (pow(health, 1.5) / 5):
+				#danger_audio_player.pitch_scale = max(0.5,health * 0.3)
+				danger_audio_player.play()
+				var damage_effect = get_node("DamageEffect")
+				damage_effect.modulate = Color(1, 0.58, 0.43,1)
+				yield(get_tree().create_timer(0.3), "timeout")
+				damage_effect.modulate = Color(1, 1, 1, 1)
+				low_health_timer = 0
+				
+	# Update health or display constant damage effect
+	if health <= 0:
+		var damage_effect = get_node("DamageEffect")
+		damage_effect.modulate = Color(1, 0.58, 0.43,1)
+		emit_signal("player_death")
+	else:
+		set_health(health - (delta * 0.2))
+			
 
 func _physics_process(delta):
 	var input_vector = get_input_vector()
@@ -83,6 +108,8 @@ func get_input_vector():
 		input_vector.y -= 1
 	if Input.is_action_pressed("move_down"):
 		input_vector.y += 1
+	if Input.is_action_just_pressed("ui_up"):
+		damage()
 	
 	# Normalize the input vector to ensure that diagonal movement is not faster than horizontal/vertical movement
 	return input_vector.normalized()
@@ -104,19 +131,30 @@ func shoot(projectile_dir: Vector2 = Vector2.ZERO, deviation_angle = 0):
 	# camera.shake()
 	camera.flash_screen()
 	
+	set_health(health - 0.1)
+	
 func damage():
+	
+	# Play damage visual effect
 	var damage_effect = get_node("DamageEffect")
 	damage_effect.modulate = Color(1, 0, 0, 1)
 	yield(get_tree().create_timer(0.1), "timeout")
 	damage_effect.modulate = Color(1, 1, 1, 1)
+	yield(get_tree().create_timer(0.05), "timeout")
+	damage_effect.modulate = Color(1, 0, 0, 1)
+	yield(get_tree().create_timer(0.1), "timeout")
+	damage_effect.modulate = Color(1, 1, 1, 1)
+	
 	set_health(health - health_increment)
 	
 func heal(health_amount:float):
-	if (health == 10): # If already full on health, don't try to increment
+	if (health >= 10): # If already full on health, don't try to increment
 		play_sound("assets/sounds/battery_collect_full.wav", 1)
 		return
 	var new_health = health + health_amount
 	set_health(new_health)
+	
+	# Play health visual effect
 	var damage_effect = get_node("DamageEffect")
 	damage_effect.modulate = Color(0, 1, 0.8, 1)
 	yield(get_tree().create_timer(0.05), "timeout")
@@ -129,7 +167,7 @@ func heal(health_amount:float):
 	# Adjust pitch based on new_health value
 	var scaled_pitch = 2 + (new_health / 50)
 	play_sound("assets/sounds/battery_collect.wav", scaled_pitch)
-	if new_health == 10:
+	if new_health >= 10:
 		play_sound("assets/sounds/health_full.wav", 2)
 
 func knockback(projectile_dir: Vector2 = Vector2.ZERO, deviation_angle: float = 0):
@@ -141,7 +179,6 @@ func play_sound(sound_path, pitch):
 	effect_audio_player.stream = collect_sound
 	effect_audio_player.pitch_scale = pitch
 	effect_audio_player.play()
-
 	
 func set_health(new_value):
 	health = min(new_value, 10)
