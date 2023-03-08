10ed4d2025e10e3b0dda9d8041fd8dd56db2e9ab
Ben Moore <ben@benhmoore.com>
2023-03-08 14:12:17 -0600

    Added balancing by improving projectile accuracy after player deaths reach a certain threshold

--- scripts/player/PlayerHUD.gd
+++ scripts/player/PlayerHUD.gd
@@ -6,7 +6,7 @@ func _on_Button_pressed():
 	var pause_menu = PauseScreen.instance()
 	add_child(pause_menu)
 
-onready var player: KinematicBody2D = get_node_or_null("/root/Node2D/PlayerCharacter")
+onready var player: KinematicBody2D = get_node_or_null("/root/Main/PlayerCharacter")
 onready var health_bar = get_node_or_null("Display/HealthBar")
 onready var current_ammo_bar = get_node_or_null("Display/CurrentAmmoBar")
 
@@ -14,7 +14,7 @@ onready var current_ammo_bar = get_node_or_null("Display/CurrentAmmoBar")
 
 func _process(_delta):
 	if not player:
-		player = get_node_or_null("/root/Node2D/PlayerCharacter")
+		player = get_node_or_null("/root/Main/PlayerCharacter")
 		if player:
 			player.connect("health_changed", self, "_on_player_health_changed")
 			player.connect("ammo_changed", self, "_on_player_ammo_changed")
